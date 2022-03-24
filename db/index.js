1a814dc9b55f29213bb22cb72604c110c9f6bfac
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-03-24 13:14:59 -0500

    Made some changes

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 const { Client } = require("pg");
+const { database } = require("pg/lib/defaults");
 
 const client = new Client("postgres://localhost:5432/juicebox-dev");
 
@@ -61,9 +62,95 @@ async function updateUser(id, fields = {}) {
   }
 }
 
+async function createPost({ authorId, title, content }) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+        INSERT INTO users(authorId, title, content) 
+        VALUES($1, $2, $3) 
+        RETURNING *;
+        `,
+      [authorId, title, content]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllPosts() {
+  try {
+    const { rows } = await client.query(
+      `SELECT id, username, name, location, active
+      FROM users;
+    `
+    );
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updatePost(id, { title, content, active }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(`,`);
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+        UPDATE users
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+      `,
+
+      Object.values(fields)
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getPostsByUser(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+      SELECT * FROM posts
+      WHERE id = ${userId}
+    `);
+    if (!user) {
+      return null;
+    }
+
+    delete user.password;
+    user.posts = await getPostsByUser(userId);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
   getAllUsers,
   createUser,
   updateUser,
+  getAllPosts,
+  createPost,
+  updatePost,
+  getPostsByUser,
 };
